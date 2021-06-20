754f50d6d86bfc1a6f34d2159825ea454acce834
Oliver Gould <ver@buoyant.io>
2021-06-20 06:28:25 +0000

    Split k8s-tests into their own workspace

---
+++ k8s-tests/src/lib.rs
@@ -0,0 +1,5 @@
+use anyhow::Result;
+
+pub async fn run(_client: kube::Client) -> Result<()> {
+    todo!()
+}
--- k8s-tests/src/main.rs
+++ k8s-tests/src/main.rs
@@ -2,5 +2,6 @@
 async fn main() -> anyhow::Result<()> {
     let _trace = linkerd_tracing::test::trace_init();
 
-    linkerd_k8s_tests::run().await
+    let client = kube::Client::try_default().await?;
+    linkerd_k8s_tests::run(client).await
 }
