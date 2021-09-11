d7fbbfbb5a712baf0f2909d63ac8afafe504af79
Oliver Gould <ver@buoyant.io>
2021-09-11 03:28:33 +0000

    fixup label tests

--- linkerd/app/inbound/src/detect.rs
+++ linkerd/app/inbound/src/detect.rs
@@ -440,9 +440,9 @@ mod tests {
                 authorizations: vec![Authorization {
                     authentication: Authentication::Unauthenticated,
                     networks: vec![client_addr().ip().into()],
-                    name: "testsaz".to_string(),
+                    name: "testsaz".into(),
                 }],
-                name: "testsrv".to_string(),
+                name: "testsrv".into(),
             },
         );
         allow
